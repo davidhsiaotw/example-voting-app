8487fc0625c78487bc24e7cb5300bc02ec058ec5
David Xiao <71328511+davidhsiaotw@users.noreply.github.com>
2026-04-28 00:15:52 -0700

    refactor(vote): change API routes

--- vote/app.py
+++ vote/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, make_response, g
+from flask import Flask, request, jsonify, make_response, g
 from redis import Redis
 import os
 import socket
@@ -21,31 +21,40 @@ def get_redis():
         g.redis = Redis(host="redis", db=0, socket_timeout=5)
     return g.redis
 
-@app.route("/", methods=['POST','GET'])
-def hello():
+@app.route("/api/options", methods=['GET'])
+def get_options():
+    return jsonify({
+        "option_a": option_a,
+        "option_b": option_b,
+        "hostname": hostname
+    })
+
+@app.route("/api/vote", methods=['POST'])
+def vote():
     voter_id = request.cookies.get('voter_id')
     if not voter_id:
         voter_id = hex(random.getrandbits(64))[2:-1]
 
-    vote = None
-
-    if request.method == 'POST':
+    data = request.get_json(silent=True)
+    vote_choice = None
+    if data and 'vote' in data:
+        vote_choice = data['vote']
+    else:
+        vote_choice = request.form.get('vote')
+    
+    if vote_choice:
         redis = get_redis()
-        vote = request.form['vote']
-        app.logger.info('Received vote for %s', vote)
-        data = json.dumps({'voter_id': voter_id, 'vote': vote})
-        redis.rpush('votes', data)
-
-    resp = make_response(render_template(
-        'index.html',
-        option_a=option_a,
-        option_b=option_b,
-        hostname=hostname,
-        vote=vote,
-    ))
+        app.logger.info('Received vote for %s', vote_choice)
+        vote_data = json.dumps({'voter_id': voter_id, 'vote': vote_choice})
+        redis.rpush('votes', vote_data)
+        
+    resp = make_response(jsonify({
+        "success": True,
+        "voter_id": voter_id,
+        "vote": vote_choice
+    }))
     resp.set_cookie('voter_id', voter_id)
     return resp
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
