b4345526357d7f972a2013bc754cf5951f7d93e0
wyxiao6 <71328511+davidhsiaotw@users.noreply.github.com>
2026-05-09 02:01:20 -0400

    feat(worker): remove table initialization for migration and scalability

--- worker/Program.cs
+++ worker/Program.cs
@@ -93,13 +93,6 @@ namespace Worker
 
             Console.Error.WriteLine("Connected to db");
 
-            var command = connection.CreateCommand();
-            command.CommandText = @"CREATE TABLE IF NOT EXISTS votes (
-                                        id VARCHAR(255) NOT NULL UNIQUE,
-                                        vote VARCHAR(255) NOT NULL
-                                    )";
-            command.ExecuteNonQuery();
-
             return connection;
         }
 
